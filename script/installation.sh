2e9f77341bc2c33104948063971d5bb1365975f1
BlakeLewis1 <blakelewis110@gmail.com>
2020-06-03 15:51:23 +0000

    adding fix

--- script/installation.sh
+++ script/installation.sh
@@ -2,5 +2,5 @@
 sudo systemctl daemon-reload
 source venv/bin/activate
 pip3 install -r requirements.txt
-source /var/lib/jenkins/workspace/flask-project/venv/bin/activate
-python3 /var/lib/jenkins/workspace/flask-project/app.py
+source /var/lib/jenkins/workspace/pipeline_test/venv/bin/activate
+python3 /var/lib/jenkins/workspace/pipeline_test/app.py
